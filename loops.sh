b3c2b66c9fba50682fda921ab1a60f6d14b44ddc
apurva <singhapurva710@gmail.com>
2023-12-10 23:00:27 +0530

    added loops script

---
+++ loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+####################
+
+
+#while loop
+
+
+
+var=1
+
+while [ $var -le 10 ]
+do
+	echo $var
+	var=$(( $var +1 ))
+	sleep 0.5
+done
+
+#for loop
+
+
+for file in logfiles/*.log
+do 
+	tar -czvf $file.tar.gz $file
+done
